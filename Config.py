d2cf402ec3f1a24b1be7c83b3b27fa437f335564
unknown <aman.grimebyte@gmail.com>
2023-02-23 15:19:51 +0530

    ok

--- Config.py
+++ Config.py
@@ -18,15 +18,15 @@ def Variable(source_name):
 
   variables =  {
     "SOURCE_NAME":facebook_table[1],
-    "SOURCE_URL" : facebook_table[2],
-    "FEATURE_IMAGE" : facebook_table[3],
-    "INTERNEL_IMAGE" : facebook_table[4],
-    "INTERNEL_IMAGE_1" : facebook_table[5],
-    "POST_TIME" : facebook_table[6],
-    "POST_TITLE" : facebook_table[7],
-    "PAGE_ID" : facebook_table[8],
-    "PAGE_TOKEN" : facebook_table[9],
-    "IMAGE_SRC": facebook_table[10],
+    "SOURCE_URL" : facebook_table[3],
+    "FEATURE_IMAGE" : facebook_table[4],
+    "INTERNEL_IMAGE" : facebook_table[5],
+    "INTERNEL_IMAGE_1" : facebook_table[6],
+    "POST_TIME" : facebook_table[7],
+    "POST_TITLE" : facebook_table[8],
+    "PAGE_ID" : facebook_table[9],
+    "PAGE_TOKEN" : facebook_table[10],
+    "IMAGE_SRC": facebook_table[11],
   }
 
   return variables,mydb
